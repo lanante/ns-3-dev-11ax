4f7a0bda7ae69805d9d4bb91e8e3ef2aa83f501b
Sébastien Deronne <sebastien.deronne@gmail.com>
2019-02-26 15:57:08 +0100

    wifi: Rename ConstantChannelBondingManager to ConstantThresholdChannelBondingManager

--- src/wifi/helper/wifi-helper.cc
+++ src/wifi/helper/wifi-helper.cc
@@ -875,7 +875,7 @@ WifiHelper::EnableLogComponents (void)
   LogComponentEnable ("BlockAckManager", LOG_LEVEL_ALL);
   LogComponentEnable ("CaraWifiManager", LOG_LEVEL_ALL);
   LogComponentEnable ("ChannelBondingManager", LOG_LEVEL_ALL);
-  LogComponentEnable ("ConstantChannelBondingManager", LOG_LEVEL_ALL);
+  LogComponentEnable ("ConstantThresholdChannelBondingManager", LOG_LEVEL_ALL);
   LogComponentEnable ("ConstantObssPdAlgorithm", LOG_LEVEL_ALL);
   LogComponentEnable ("ConstantRateWifiManager", LOG_LEVEL_ALL);
   LogComponentEnable ("Txop", LOG_LEVEL_ALL);
