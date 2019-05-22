e398e6f14eec4f951dc94651e5cff1d290f3e55d
root <leonardolanante@gmail.com>
2019-05-22 23:33:48 +0900

    Added Sim0.sh

--- src/wifi-he/scripts/Sim0.sh
+++ src/wifi-he/scripts/Sim0.sh
@@ -11,14 +11,15 @@ export standard=11ax_5GHZ
 obssPdLevel=-82
 #export NS_LOG=DynamicObssPdAlgorithm=logic
 
-for i in 1 2 3 4 5; do
-for j in 0 5 11;do
-for k in 2 3 4 5 6 7 ; do
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
+for j in -82 -80 -78 -76 -74 -72 -70 -68 -66 -64 -62 -;do
+for k in 2 ; do
 RngRunpar=${i}
 nBsspar=${k}
-MCSpar=${j}
+MCSpar=5
 npar=10
 durpar=5
+obsspdpar=${j}
 if [ ${MCSpar} -eq 0 ]
 then
 ampdusizepar=4915
@@ -68,13 +69,14 @@ then
 ampdusizepar=65535
 fi
 
-Res=Sim1a
+Res=Sim0a
 dlpar=100
 ulpar=0
-export RngRun=${RngRunpar}
+
 export powerBackoff=0
-export obssPdAlgorithm=DynamicObssPdAlgorithm
-export obssPdThreshold=-82
+export RngRun=${RngRunpar}
+export obssPdAlgorithm=ConstantObssPdAlgorithm
+export obssPdThreshold=${obsspdpar}
 export maxAmpduSize=${ampdusizepar}
 export MCS=${MCSpar}
 export downlink=${dlpar}
@@ -112,11 +114,10 @@ export test=${Res}_${obssPdAlgorithm}_${obssPdThreshold}_${nBss}_${enableObssPd}
 run_one &
 sleep 1
 
-
-export powerBackoff=0
+export powerBackoff=1
 export RngRun=${RngRunpar}
 export obssPdAlgorithm=ConstantObssPdAlgorithm
-export obssPdThreshold=-82
+export obssPdThreshold=${obsspdpar}
 export maxAmpduSize=${ampdusizepar}
 export MCS=${MCSpar}
 export downlink=${dlpar}
@@ -154,94 +155,13 @@ export test=${Res}_${obssPdAlgorithm}_${obssPdThreshold}_${nBss}_${enableObssPd}
 run_one &
 sleep 1
 
-export powerBackoff=0
-export RngRun=${RngRunpar}
-export obssPdAlgorithm=ConstantObssPdAlgorithm
-export obssPdThreshold=-72
-export maxAmpduSize=${ampdusizepar}
-export MCS=${MCSpar}
-export downlink=${dlpar}
-export uplink=${ulpar}
-export duration=${durpar}
-export enableRts=0
-export txStartOffset=50
-export enableObssPd=1
-export txGain=0
-export rxGain=0
-export antennas=1
-export maxSupportedTxSpatialStreams=1
-export maxSupportedRxSpatialStreams=1
-export performTgaxTimingChecks=0
-export nodePositionsFile=NONE
-export bw=20
-export scenario=logdistance
-export nBss=${nBsspar}
-export payloadSizeUplink=1500
-export payloadSizeDownlink=1500
-export useIdealWifiManager=0
-export r=5
-export powSta=20
-export powAp=20
-export txRange=102
-export ccaTrSta=-62
-export ccaTrAp=-62
-export d=20
-export sigma=3.5
-export bianchi=1
-export n=${npar}
-export enableObssPd=1
-export test=Res_${obssPdAlgorithm}_${obssPdThreshold}
-export test=${Res}_${obssPdAlgorithm}_${obssPdThreshold}_${nBss}_${enableObssPd}_${MCS}_${powerBackoff}_${d}_${RngRun}
-run_one &
-sleep 1
-
-export powerBackoff=0
-export RngRun=${RngRunpar}
-export obssPdAlgorithm=ConstantObssPdAlgorithm
-export obssPdThreshold=-62
-export maxAmpduSize=${ampdusizepar}
-export MCS=${MCSpar}
-export downlink=${dlpar}
-export uplink=${ulpar}
-export duration=${durpar}
-export enableRts=0
-export txStartOffset=50
-export enableObssPd=1
-export txGain=0
-export rxGain=0
-export antennas=1
-export maxSupportedTxSpatialStreams=1
-export maxSupportedRxSpatialStreams=1
-export performTgaxTimingChecks=0
-export nodePositionsFile=NONE
-export bw=20
-export scenario=logdistance
-export nBss=${nBsspar}
-export payloadSizeUplink=1500
-export payloadSizeDownlink=1500
-export useIdealWifiManager=0
-export r=5
-export powSta=20
-export powAp=20
-export txRange=102
-export ccaTrSta=-62
-export ccaTrAp=-62
-export d=20
-export sigma=3.5
-export bianchi=1
-export n=${npar}
-export enableObssPd=1
-export test=Res_${obssPdAlgorithm}_${obssPdThreshold}
-export test=${Res}_${obssPdAlgorithm}_${obssPdThreshold}_${nBss}_${enableObssPd}_${MCS}_${powerBackoff}_${d}_${RngRun}
-run_one &
-
-Res=Sim1b
+Res=Sim0b
 dlpar=0
 ulpar=100
 export powerBackoff=0
 export RngRun=${RngRunpar}
-export obssPdAlgorithm=DynamicObssPdAlgorithm
-export obssPdThreshold=-82
+export obssPdAlgorithm=ConstantObssPdAlgorithm
+export obssPdThreshold=${obsspdpar}
 export maxAmpduSize=${ampdusizepar}
 export MCS=${MCSpar}
 export downlink=${dlpar}
@@ -279,94 +199,10 @@ export test=${Res}_${obssPdAlgorithm}_${obssPdThreshold}_${nBss}_${enableObssPd}
 run_one &
 sleep 1
 
-
-export powerBackoff=0
+export powerBackoff=1
 export RngRun=${RngRunpar}
 export obssPdAlgorithm=ConstantObssPdAlgorithm
-export obssPdThreshold=-82
-export maxAmpduSize=${ampdusizepar}
-export MCS=${MCSpar}
-export downlink=${dlpar}
-export uplink=${ulpar}
-export duration=${durpar}
-export enableRts=0
-export txStartOffset=50
-export enableObssPd=1
-export txGain=0
-export rxGain=0
-export antennas=1
-export maxSupportedTxSpatialStreams=1
-export maxSupportedRxSpatialStreams=1
-export performTgaxTimingChecks=0
-export nodePositionsFile=NONE
-export bw=20
-export scenario=logdistance
-export nBss=${nBsspar}
-export payloadSizeUplink=1500
-export payloadSizeDownlink=1500
-export useIdealWifiManager=0
-export r=5
-export powSta=20
-export powAp=20
-export txRange=102
-export ccaTrSta=-62
-export ccaTrAp=-62
-export d=20
-export sigma=3.5
-export bianchi=1
-export n=${npar}
-export enableObssPd=1
-export test=Res_${obssPdAlgorithm}_${obssPdThreshold}
-export test=${Res}_${obssPdAlgorithm}_${obssPdThreshold}_${nBss}_${enableObssPd}_${MCS}_${powerBackoff}_${d}_${RngRun}
-run_one &
-sleep 1
-
-export powerBackoff=0
-export RngRun=${RngRunpar}
-export obssPdAlgorithm=ConstantObssPdAlgorithm
-export obssPdThreshold=-72
-export maxAmpduSize=${ampdusizepar}
-export MCS=${MCSpar}
-export downlink=${dlpar}
-export uplink=${ulpar}
-export duration=${durpar}
-export enableRts=0
-export txStartOffset=50
-export enableObssPd=1
-export txGain=0
-export rxGain=0
-export antennas=1
-export maxSupportedTxSpatialStreams=1
-export maxSupportedRxSpatialStreams=1
-export performTgaxTimingChecks=0
-export nodePositionsFile=NONE
-export bw=20
-export scenario=logdistance
-export nBss=${nBsspar}
-export payloadSizeUplink=1500
-export payloadSizeDownlink=1500
-export useIdealWifiManager=0
-export r=5
-export powSta=20
-export powAp=20
-export txRange=102
-export ccaTrSta=-62
-export ccaTrAp=-62
-export d=20
-export sigma=3.5
-export bianchi=1
-export n=${npar}
-export enableObssPd=1
-export test=Res_${obssPdAlgorithm}_${obssPdThreshold}
-export test=${Res}_${obssPdAlgorithm}_${obssPdThreshold}_${nBss}_${enableObssPd}_${MCS}_${powerBackoff}_${d}_${RngRun}
-run_one &
-sleep 1
-
-
-export powerBackoff=0
-export RngRun=${RngRunpar}
-export obssPdAlgorithm=ConstantObssPdAlgorithm
-export obssPdThreshold=-62
+export obssPdThreshold=${obsspdpar}
 export maxAmpduSize=${ampdusizepar}
 export MCS=${MCSpar}
 export downlink=${dlpar}
