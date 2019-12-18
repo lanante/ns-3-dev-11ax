66159330bb9b5fdff984e3b2ccb7aacabd07c75f
Leonardo <leonardolanante@gmail.com>
2019-12-17 17:09:06 -0800

    added DT-DCB.sh

---
+++ scratch/DT-DCB.sh
@@ -0,0 +1,66 @@
+
+channelBssA=42
+channelBssB=40
+channelBssC=46
+channelBssD=42
+channelBssE=42
+channelBssF=42
+channelBssG=42
+
+primaryChannelBssA=36
+primaryChannelBssB=40
+primaryChannelBssC=48
+primaryChannelBssD=48
+primaryChannelBssE=36
+primaryChannelBssF=40
+primaryChannelBssG=44
+
+uplinkA=200
+uplinkB=200
+uplinkC=200
+uplinkD=200
+uplinkE=200
+uplinkF=200
+uplinkG=200
+downlinkA=0
+downlinkB=0
+downlinkC=0
+downlinkD=0
+downlinkE=0
+downlinkF=0
+downlinkG=0
+
+payloadSize=1472
+simulationTime=5
+
+
+ccaEdThresholdPrimary=-62
+constantCcaEdThresholdSecondaryBss=-1000
+channelBondingType=ConstantThreshold
+interBssDistance=10
+distance=10;
+n=10
+nBss=3
+RngRun=1
+mcs=MaxMcs
+
+
+../waf
+
+for RngRun in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40;
+do
+for interBssDistance in 10 20 40;
+do
+	for constantCcaEdThresholdSecondaryBss in -88 -85 -82 -79 -76 -73 -70 -67 -64 -61 -58 -55 -52; 
+
+do
+				Test=${nBss}_${n}_${interBssDistance}_${channelBondingType}_${constantCcaEdThresholdSecondaryBss}_${mcs}_${RngRun}
+				echo "Starting $Test"
+../waf --run "channel-bonding --Test=$Test --mcs=$mcs --nBss=$nBss --simulationTime=$simulationTime --RngRun=$RngRun --uplinkA=$uplinkA --uplinkB=$uplinkB --uplinkC=$uplinkC --uplinkD=$uplinkD --uplinkE=$uplinkE --uplinkF=$uplinkF --uplinkG=$uplinkG --downlinkA=$downlinkA --downlinkB=$downlinkB --downlinkC=$downlinkC --downlinkD=$downlinkD --downlinkE=$downlinkE --downlinkF=$downlinkF --downlinkG=$downlinkG --channelBondingType=$channelBondingType  --n=$n --interBssDistance=$interBssDistance --distance=$distance --ccaEdThresholdPrimaryBssA=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssB=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssC=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssD=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssE=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssF=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssG=$ccaEdThresholdPrimary --ccaEdThresholdSecondaryBssA=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssB=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssC=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssD=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssE=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssF=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssG=$constantCcaEdThresholdSecondaryBss  --channelBssA=$channelBssA --channelBssB=$channelBssB --channelBssC=$channelBssC --channelBssD=$channelBssD --channelBssE=$channelBssE --channelBssF=$channelBssF --channelBssG=$channelBssG --primaryChannelBssA=$primaryChannelBssA --primaryChannelBssB=$primaryChannelBssB --primaryChannelBssC=$primaryChannelBssC --primaryChannelBssD=$primaryChannelBssD --primaryChannelBssE=$primaryChannelBssE --primaryChannelBssF=$primaryChannelBssF --primaryChannelBssG=$primaryChannelBssG" &
+sleep 0.1
+		done
+wait
+	done
+done
+
+
