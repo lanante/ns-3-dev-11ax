2e3283e0d45b76065fe127c56a5003dd1fae22e1
Leonardo Lanante <leonardolanante@gmail.com>
2020-11-12 20:31:26 +0900

    Increase simulation time and reps of Sim1.sh

--- scratch/Sim1.sh
+++ scratch/Sim1.sh
@@ -13,7 +13,7 @@ downlinkA=140
 downlinkB=10
 
 payloadSize=1472
-simulationTime=5
+simulationTime=50
 
 
 ccaEdThresholdPrimary=-62
@@ -30,13 +30,14 @@ mcs2=VhtMcs0
 ../waf
 
 for mcs1 in VhtMcs0 VhtMcs4 VhtMcs8 ; do
-for constantCcaEdThresholdSecondaryBss in -87 -85 -83 -81 -79 -77 -75 -73 -71 -69 -67 -65 -63 -61 -59 -57 ;
-do
-for RngRun_j in 0 10 20 30 40 50 60 70 80 90; do
-for RngRun_i in 1 2 3 4 5 6 7 8 9 10; do
-RngRun=$((RngRun_j+RngRun_i))
+for constantCcaEdThresholdSecondaryBss in {-87..-57..2} ; do
+for RngRun_k in {0..900..100}; do
+for RngRun_j in {10..90..10}; do
+for RngRun_i in {1..10}; do
+
+RngRun=$((${RngRun_k}+${RngRun_j}+${RngRun_i}))
 Test=${nBss}_${n}_${interBssDistance}_${channelBondingType}_${constantCcaEdThresholdSecondaryBss}_${mcs1}_${RngRun}
-				echo "Starting $Test"
+				echo Starting $Test
 ../waf --run "channel-bonding --Test=$Test --mcs1=$mcs1 --mcs2=$mcs2 --nBss=$nBss --simulationTime=$simulationTime --RngRun=$RngRun --uplinkA=$uplinkA --uplinkB=$uplinkB  --downlinkA=$downlinkA --downlinkB=$downlinkB --channelBondingType=$channelBondingType  --n=$n --interBssDistance=$interBssDistance --distance=$distance --ccaEdThresholdPrimaryBssA=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssB=$ccaEdThresholdPrimary  --ccaEdThresholdSecondaryBssA=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssB=$constantCcaEdThresholdSecondaryBss   --channelBssA=$channelBssA --channelBssB=$channelBssB  --primaryChannelBssA=$primaryChannelBssA --primaryChannelBssB=$primaryChannelBssB" &
 sleep 0.1
 	done
@@ -44,20 +45,21 @@ wait
 done
 done
 done
-
+done
 
 for mcs1 in VhtMcs0 VhtMcs1 VhtMcs2 VhtMcs3 VhtMcs4 VhtMcs5 VhtMcs6 VhtMcs7 VhtMcs8 ; do
-for constantCcaEdThresholdSecondaryBss in -82 -72 -62 ;
-do
-for RngRun_j in 0 10 20 30 40 50 60 70 80 90; do
-for RngRun_i in 1 2 3 4 5 6 7 8 9 10; do
-RngRun=$((RngRun_j+RngRun_i))
+for constantCcaEdThresholdSecondaryBss in -82 -72 -62 ; do
+for RngRun_k in {0..900..100}; do
+for RngRun_j in {0..90..10}; do
+for RngRun_i in {1..10}; do
+RngRun=$((${RngRun_k}+${RngRun_j}+${RngRun_i}))
 Test=${nBss}_${n}_${interBssDistance}_${channelBondingType}_${constantCcaEdThresholdSecondaryBss}_${mcs1}_${RngRun}
-				echo "Starting $Test"
+				echo Starting $Test 
 ../waf --run "channel-bonding --Test=$Test --mcs1=$mcs1 --mcs2=$mcs2 --nBss=$nBss --simulationTime=$simulationTime --RngRun=$RngRun --uplinkA=$uplinkA --uplinkB=$uplinkB  --downlinkA=$downlinkA --downlinkB=$downlinkB --channelBondingType=$channelBondingType  --n=$n --interBssDistance=$interBssDistance --distance=$distance --ccaEdThresholdPrimaryBssA=$ccaEdThresholdPrimary --ccaEdThresholdPrimaryBssB=$ccaEdThresholdPrimary  --ccaEdThresholdSecondaryBssA=$constantCcaEdThresholdSecondaryBss --ccaEdThresholdSecondaryBssB=$constantCcaEdThresholdSecondaryBss   --channelBssA=$channelBssA --channelBssB=$channelBssB  --primaryChannelBssA=$primaryChannelBssA --primaryChannelBssB=$primaryChannelBssB" &
 sleep 0.1
 	done
 wait
+done
 done
 done
 done
