cc502002a0ba504f34299aef66e7e8d19087f1ab
leo <leonardolanante@gmail.com>
2021-02-16 13:13:24 +0900

    Saveing IWP

--- scratch/multibss.sh
+++ scratch/multibss.sh
@@ -4,16 +4,16 @@
 standard=11ac
 enableFrameCapture=true
 RngRun=1
-enableThresholdPreambleDetection=false
+enableThresholdPreambleDetection=true
 maxAmpduSize=65535
 MCS=8
 downlink=0
 uplink=100
-duration=10
+duration=5
 bw=20
 nBss=2
-payloadSizeUplink=1500
-payloadSizeDownlink=1500
+payloadSizeUplink=1000
+payloadSizeDownlink=1000
 
 r=8
 powSta=20
@@ -23,13 +23,24 @@ powAp=20
 d=15
 bianchi=1
 n=10
-fixedPosition=0
+fixedPosition=1
 obssMCS=0
 ccaTr=-62
+
+../waf
+rm Result*
+for RngRun in {1..4};do
+
 ccaTrObss=-82
-echo Known
-../waf --run "spatial-reuse --bianchi=${bianchi} --enableThresholdPreambleDetection=${enableThresholdPreambleDetection} --ccaTrStaObss=${ccaTrObss} --ccaTrApObss=${ccaTrObss} --ccaTrSta=${ccaTr} --ccaTrAp=${ccaTr}  --enableFrameCapture=${enableFrameCapture} --RngRun=${RngRun} --payloadSizeUplink=${payloadSizeUplink} --payloadSizeDownlink=${payloadSizeDownlink} --r=${r} --powSta=${powSta} --powAp=${powAp} --d=${d} --n=${n} --fixedPosition=${fixedPosition} --uplink=${uplink} --downlink=${downlink} --duration=${duration} --obssMCS=${obssMCS} --MCS=${MCS} --nBss=${nBss} --maxAmpduSizeBss1=${maxAmpduSize} --maxAmpduSizeBss2=${maxAmpduSize}" 
+outputFile=Result_${ccaTrObss}_${RngRun}
+echo Starting $outputFile
+../waf --run "spatial-reuse --bianchi=${bianchi} --enableThresholdPreambleDetection=${enableThresholdPreambleDetection} --ccaTrStaObss=${ccaTrObss} --ccaTrApObss=${ccaTrObss} --ccaTrSta=${ccaTr} --ccaTrAp=${ccaTr}  --enableFrameCapture=${enableFrameCapture} --RngRun=${RngRun} --payloadSizeUplink=${payloadSizeUplink} --payloadSizeDownlink=${payloadSizeDownlink} --r=${r} --powSta=${powSta} --powAp=${powAp} --d=${d} --n=${n} --fixedPosition=${fixedPosition} --uplink=${uplink} --downlink=${downlink} --duration=${duration} --obssMCS=${obssMCS} --MCS=${MCS} --nBss=${nBss} --maxAmpduSizeBss1=${maxAmpduSize} --maxAmpduSizeBss2=${maxAmpduSize}" >> ${outputFile} &
 
 ccaTrObss=-62
-echo Unknown
-../waf --run "spatial-reuse --bianchi=${bianchi} --enableThresholdPreambleDetection=${enableThresholdPreambleDetection} --ccaTrStaObss=${ccaTrObss} --ccaTrApObss=${ccaTrObss} --ccaTrSta=${ccaTr} --ccaTrAp=${ccaTr}  --enableFrameCapture=${enableFrameCapture} --RngRun=${RngRun} --payloadSizeUplink=${payloadSizeUplink} --payloadSizeDownlink=${payloadSizeDownlink} --r=${r} --powSta=${powSta} --powAp=${powAp} --d=${d} --n=${n} --fixedPosition=${fixedPosition} --uplink=${uplink} --downlink=${downlink} --duration=${duration} --obssMCS=${obssMCS} --MCS=${MCS} --nBss=${nBss} --maxAmpduSizeBss1=${maxAmpduSize} --maxAmpduSizeBss2=${maxAmpduSize}" 
+outputFile=Result_${ccaTrObss}_${RngRun}
+echo Starting $outputFile
+../waf --run "spatial-reuse --bianchi=${bianchi} --enableThresholdPreambleDetection=${enableThresholdPreambleDetection} --ccaTrStaObss=${ccaTrObss} --ccaTrApObss=${ccaTrObss} --ccaTrSta=${ccaTr} --ccaTrAp=${ccaTr}  --enableFrameCapture=${enableFrameCapture} --RngRun=${RngRun} --payloadSizeUplink=${payloadSizeUplink} --payloadSizeDownlink=${payloadSizeDownlink} --r=${r} --powSta=${powSta} --powAp=${powAp} --d=${d} --n=${n} --fixedPosition=${fixedPosition} --uplink=${uplink} --downlink=${downlink} --duration=${duration} --obssMCS=${obssMCS} --MCS=${MCS} --nBss=${nBss} --maxAmpduSizeBss1=${maxAmpduSize} --maxAmpduSizeBss2=${maxAmpduSize}" >> ${outputFile} &
+
+done
+
+wait
